ed367957bf3a114c3ebd4ff0e6d39b6d8e2cea00
chessMan <71754779+mohitmishra786@users.noreply.github.com>
2024-09-15 11:34:21 +0530

    Create visPaddingWithPragma.c

---
+++ AccessPatterns/visPaddingWithPragma.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#pragma pack(push, 1)
+struct GamePacket {
+    uint8_t packetType;
+    uint32_t playerId;
+    float positionX;
+    float positionY;
+    uint8_t health;
+    uint16_t score;
+};
+#pragma pack(pop)
+
+int main() {
+    struct GamePacket packet;
+    
+    printf("Address of packetType: %p\n", (void*)&packet.packetType);
+    printf("Address of playerId:   %p\n", (void*)&packet.playerId);
+    printf("Address of positionX:  %p\n", (void*)&packet.positionX);
+    printf("Address of positionY:  %p\n", (void*)&packet.positionY);
+    printf("Address of health:     %p\n", (void*)&packet.health);
+    printf("Address of score:      %p\n", (void*)&packet.score);
+    
+    printf("Size of GamePacket: %zu bytes\n", sizeof(struct GamePacket));
+    return 0;
+}
