3427ed9c6f7772404eb71c4672c70050bcabcbbf
chessMan <71754779+mohitmishra786@users.noreply.github.com>
2024-09-15 11:34:39 +0530

    Update visPadding.c

--- AccessPatterns/visPadding.c
+++ AccessPatterns/visPadding.c
@@ -11,6 +11,15 @@ struct GamePacket {
 };
 
 int main() {
+    struct GamePacket packet;
+    
+    printf("Address of packetType: %p\n", (void*)&packet.packetType);
+    printf("Address of playerId:   %p\n", (void*)&packet.playerId);
+    printf("Address of positionX:  %p\n", (void*)&packet.positionX);
+    printf("Address of positionY:  %p\n", (void*)&packet.positionY);
+    printf("Address of health:     %p\n", (void*)&packet.health);
+    printf("Address of score:      %p\n", (void*)&packet.score);
+    
     printf("Size of GamePacket: %zu bytes\n", sizeof(struct GamePacket));
     return 0;
 }
